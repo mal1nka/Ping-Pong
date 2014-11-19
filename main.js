eaf0d33c6b223db7a3d3bb74ce24b00ace69df03
amelnyk <alina.melnyk22@gmail.com>
2014-11-19 17:20:32 +0200

    move players

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function Base(color, x, y, canvasContext) {
 
 Base.prototype = {
 	draw: function() {
-		this.canvasContext.fillStyle = this.color; // меняем цвет клеток, fillStyle = color определяет цвет заливки 
+		this.canvasContext.fillStyle = this.color;
 	}
 };
 
@@ -16,7 +16,7 @@ function Rectangle(color, x, y, width, height, canvasContext){
 	this.width = width;
 	this.height = height;
 	Base.call(this, color, x, y, canvasContext);
-	
+
 };
 
 Rectangle.prototype.draw = function() {
@@ -27,7 +27,7 @@ Rectangle.prototype.draw = function() {
 function Ball(color, x, y, radius, canvasContext){
 	this.radius = radius;
 	Base.call(this, color, x, y, canvasContext);
-	
+
 };
 
 
@@ -54,66 +54,84 @@ Game.prototype = {
 		canvas.height = this.height;
 		this.context = canvas.getContext('2d');
 		this.initGameObjects();
-        canvas.onmousemove = this.playerMove;
-        this.play()
-        //setInterval(this.play, 1000 / 50);
-		//this.drawGame();
-		
+        this.setupEventListener();
+        this.drawGame();
+        //this.update()
+
 	},
 	initGameObjects: function() {
-	// объект который задаёт игровое поле
 		this.game = new Rectangle("#000", 0 , 0 , this.width, this.height, this.context);
-	// Ракетки-игроки	
-		this.ai = new Rectangle("#fff", 10, this.game.height / 2 - 40, 20, 80, this.context);
-		this.player = new Rectangle("#fff", this.game.width - 30, this.game.height / 2 - 40, 20, 80, this.context);
-	// количество очков
-		this.ai.scores = 0;
-		this.player.scores = 0;
-	 // наш квадратный игровой "шарик"
+		this.playerLeft = new Rectangle("#fff", 10, this.game.height / 2 - 40, 20, 80, this.context);
+		this.playerRight = new Rectangle("#fff", this.game.width - 30, this.game.height / 2 - 40, 20, 80, this.context);
+        console.log (this.playerRight)
+
+		this.playerLeft.scores = 0;
+		this.playerRight.scores = 0;
+        this.speedPlayers = 10;
 		this.ball = new Ball("#fff" , 40, this.game.height / 2 - 10, 10, this.context);
-	// скорость шарика
-        this.ball.vX = 2; // скорость по оси х
-        this.ball.vY = 2; // скорость по оси у
 
-
+        this.ball.vX = 2;
+        this.ball.vY = 2;
 	},
+    setupEventListener: function (){
+        document.body.onkeydown = function (e) {
+            var player;
+            var direction;
+            switch (e.keyCode) {
+                case 38:
+                    player= this.playerRight;
+                    direction=-1;
+                    break;
+                case 40:
+                    player=this.playerRight;
+                    direction=1;
+                    break;
+                case 87:
+                    player=this.playerLeft;
+                    direction=-1;
+                    break;
+                case 83:
+                    player=this.playerLeft;
+                    direction=1;
+                    break;
+                default: return
+            }
+            this.startPlayerMove(direction, player)
+        }.bind(this)
+    },
 	drawGame: function() {
-		this.game.draw(); // рисуем игровое поле
-		// рисуем на поле счёт
+		this.game.draw();
 		this.context.font = 'bold 128px courier';
 		this.context.textAlign = 'center';
 		this.context.textBaseline = 'top';
 		this.context.fillStyle = '#ccc';
-		TextFactory.drawText(this.context, this.ai.scores, 100, 0);
-		TextFactory.drawText(this.context, this.player.scores, this.game.width-100, 0); 
-		for (var i = 25; i < this.game.height; i += 45) // линия разделяющая игровое поле на две части
+		TextFactory.drawText(this.context, this.playerLeft.scores, 100, 0);
+		TextFactory.drawText(this.context, this.playerRight.scores, this.game.width-100, 0);
+		for (var i = 25; i < this.game.height; i += 45)
 		   {
 				var dot = new Ball("#ccc", this.game.width/2 - 10, i, 10, this.context);
 				dot.draw();
 			}
-		this.ai.draw(); // рисуем левого игрока
-		this.player.draw();// правого игрока
-		this.ball.draw();// шарик
+		this.playerLeft.draw();
+		this.playerRight.draw();
+		this.ball.draw();
+
 	},
 
-    playerMove: function (e) {
-        var y = e.pageY;
-        if (this.player.height / 2 + 10 < y && y < this.game.height - this.player.height / 2 - 10) {
-            this.player.y = y - this.player.height / 2;
+    startPlayerMove: function (direction, player) {
+        if (player.y > 0 && direction == -1 || player.y + player.height < this.game.height && direction==1) {
+            player.y = player.y+direction*this.speedPlayers;
         }
+        this.drawGame();
+
+
     },
 
-    // Изменения которые нужно произвести
+
     update: function () {
-        // меняем координаты шарика
         this.ball.x += this.ball.vX;
         this.ball.y += this.ball.vY;
 
-    },
-
-    play: function() {
-        this.drawGame(); // отрисовываем всё на холсте
-        this.update(); // обновляем координаты
     }
 
 };
